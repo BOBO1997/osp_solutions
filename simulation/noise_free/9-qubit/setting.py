c723a7d94928a0b1b638fccd7fc39dbb1f41448d
Bo Yang <yh198595@gmail.com>
2025-10-19 16:11:05 +0200

    added 9-qubit noise-free simulation

---
+++ simulation/noise_free/9-qubit/setting.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+p_dep1 = 0 # 1.0 * 1e-4
+p_dep2 = 0 # 1.0 * 1e-3
+
+num_qubits = 9
+
+time_evolution = np.pi
+
+initial_layout = list(range(num_qubits))
+
+num_steps_list = list(range(4,100,4))
+
+state_initial_str = "110100110" # "101010101" ### specify initial state, 10101, 11100
+
+lmd = 1.0
