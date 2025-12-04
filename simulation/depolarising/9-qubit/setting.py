858b2aedca731e81086418485c49dd6547f9332d
Bo Yang <yh198595@gmail.com>
2025-12-04 18:49:20 +0000

    refined code, put noise model to setting.py, added visualise_rescaled.py

--- simulation/depolarising/9-qubit/setting.py
+++ simulation/depolarising/9-qubit/setting.py
@@ -15,3 +15,37 @@ state_initial_str = "110100110"
 # state_initial_str = "101010101" ### specify initial state, 10101, 11100
 
 lmd = 1.0
+
+
+### ====== noise model ====== ###
+
+
+from qiskit_aer import AerSimulator
+from qiskit_aer.noise import NoiseModel, pauli_error
+# from qiskit_ibm_runtime.fake_provider import FakeJakartaV2
+# backend = AerSimulator.from_backend(FakeJakartaV2())
+# backend = Aer.get_backend("qasm_simulator")
+###? p_dep1 = 1.0 * 1e-4
+error_dep1 = pauli_error([("I", 1 - 3 * p_dep1 / 4), ("X", p_dep1 / 4), ("Y", p_dep1 / 4), ("Z", p_dep1 / 4)])
+# error_dep2_local = error_dep1.tensor(error_dep1)
+
+###? p_dep2 = 1.0 * 1e-3
+error_dep2_global = pauli_error([("II", 1 - 15 * p_dep2 / 16), ("IX", p_dep2 / 16), ("IY", p_dep2 / 16), ("IZ", p_dep2 / 16),
+                                 ("XI", p_dep2 / 16), ("XX", p_dep2 / 16), ("XY", p_dep2 / 16), ("XZ", p_dep2 / 16),
+                                 ("YI", p_dep2 / 16), ("YX", p_dep2 / 16), ("YY", p_dep2 / 16), ("YZ", p_dep2 / 16),
+                                 ("ZI", p_dep2 / 16), ("ZX", p_dep2 / 16), ("ZY", p_dep2 / 16), ("ZZ", p_dep2 / 16)])
+
+# print(error_dep2_global)
+
+# Add errors to noise model
+noise_model = NoiseModel()
+noise_model.add_all_qubit_quantum_error(error_dep1, ["rx", "rz", "sx", "h", "sdg", "s", "x", "u1", "u2", "u3"])
+noise_model.add_all_qubit_quantum_error(error_dep2_global, ["cx", "cz"])
+# noise_model.add_all_qubit_quantum_error(error_3, ["cswap", "ccx"])
+print(noise_model)
+print()
+
+# Create noisy simulator backend
+simulator_noisy = AerSimulator(method="density_matrix",
+                               noise_model=noise_model)
+simulator_ideal = AerSimulator(method="density_matrix")
